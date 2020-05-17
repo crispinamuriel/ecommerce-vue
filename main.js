6faaccc2ca1de7cdbe65a72fac95a0a239638c0b
crispinamuriel <crispinamuriel@gmail.com>
2020-05-16 21:13:43 -0400

    starting to turn html structure into a Vue Component

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 /* eslint-disable quotes */
-console.log('hi');
 
 const app = new Vue({
   el: "#app",
   data: {
     brand: "Crispina's",
-    onSale: false,
+    onSale: true,
     product: "Socks",
     description: "Comfortable stylish men's socks!",
     selectedVariant: 0,
@@ -57,6 +56,9 @@ const app = new Vue({
     },
     inStock() {
       return this.variants[this.selectedVariant].variantQuantity > 0;
+    },
+    nowOnSale() {
+      return `${this.brand} ${this.product} now on sale!`
     }
   },
 });
@@ -112,5 +114,100 @@ with that in mind it's more efficient to use a computed property rather than a m
 ------------------
 <a :href="link">Other styles</a>
 when we use :href="link" we are data binding and using a dynamic variable 'link' to access some data as an expression
+
+----------------
+-Components
+
+components are reusable blocks of code used to create are more modular and maintainable code base
+
+-Nested Components
+
+components may contain several components within itself
+
+- Creating a Component
+
+-Register the component:
+
+Vue.component('componentName', {options Object});
+
+- Component Options Object
+
+nearly identical to the object inserted when creating the Vue instance
+
+- Component Template Property
+
+Vue.component('componentName', {
+    template: `<h1>I'm a component</h1>`
+});
+
+Instead of plugging into an element in the DOM like we do in the Vue instance, a component has a template property. Which specifies the structure of your component. There are different ways to construct a template but for now we can use a template literal using backticks.
+
+-  Template Root Element
+
+Warning: Component template should contain exactly one root element.
+
+Vue.component('componentName', {
+    template: `<h1>I'm a component</h1>
+               <h2>Arn't I beautiful?</h2>`
+});
+
+The backtics will allow us to add another element but the template needs to be wrapped in a div. This will allow us to have a single parent element for all of the elements in our template. This will avoid the warning: Component template should contain exactly one root element.
+
+Vue.component('componentName', {
+    template: `<div>
+                  <h1>I'm a component</h1>
+                  <h2>Arn't I beautiful?</h2>
+               </div>`
+});
+
+-Component Data function
+
+Just like the Vue instance our component can contain data as well. But notice here that data is a function that returns a data object. If it was just a data object and we were using that component throughout our app, each  copy of that component would have the same data object with the same data and would not be dynamic.
+
+Using  a data function will return a fresh data object for each component. This will give each component instance it's own uniuqe data.
+
+- Passing Props
+
+What if our component is nested somewhere, inside a parent component which has some message data. since our component has it's own isolated scope, it cannot reach up outside of itself for it's parents message data.
+
+- Props
+
+Instead we can use props to pass data down to the child component. A prop is a custom attribute for passing data into our components.
+
+In order to recieve props, a component needs to explicitly declare the props it expects to recieve in the props option. So here we're saying that our product component will recieve the prop called 'message'
+
+Vue.component('product', {
+  props:[message],
+  template: `<div>
+                {{message}}
+             </div>`
+});
+
+now we can use message in our components template
+
+- Parent passes the props down to child component
+
+      <product message="hello!"></product>
+
+- Prop Validation
+
+When declaring your component's props it is recommended to specify requirements with vue's built-in prop validation. In order to do this we can switch our props array into a props object.
+
+- props Object
+
+Vue.component('product', {
+    props: {
+        message: {
+            type: String,
+            required: true,
+            default: "Hi"
+        }
+    }
+});
+
+
+Inside here we can make an object for our message prop, and specify it's data type, if it's required or not, or give it a default value.
+
+-------------
 
 */
